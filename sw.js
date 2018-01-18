a61248360b30f872171ae4881c104c2768f6b10c
Gerhard Neugschwandtner <gerhard.neugschwandtner@gmail.com>
2018-01-18 17:21:37 +0100

    cleaned up ServiceWorker logs

--- sw.js
+++ sw.js
@@ -51,7 +51,6 @@ self.addEventListener('activate', function (event) {
 function isInArray(string, array) {
     var cachePath;
     if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
-        console.log('matched ', string);
         cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
     } else {
         cachePath = string; // store the full request (for CDNs)
